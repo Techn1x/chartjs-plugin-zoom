0d5e4674fa9050194105b25b652881ef151a8e27
Brad Overton <brad.overton@lsmtechnologies.com.au>
2019-01-24 02:15:09 +0000

    Gulped

--- chartjs-plugin-zoom.js
+++ chartjs-plugin-zoom.js
@@ -211,11 +211,11 @@ function doZoom(chartInstance, zoomX, zoomY, center, whichAxes) {
 
 		if (typeof zoomOptions.onZoom === 'function') {
 			zoomOptions.onZoom({
-				chartInstance: chartInstance,
+				chart: chartInstance,
 				zoomX: zoomX,
 				zoomY: zoomY,
 				center: center,
-				_whichAxes: _whichAxes
+				whichAxes: _whichAxes
 			});
 		}
 	}
@@ -295,7 +295,7 @@ function doPan(chartInstance, deltaX, deltaY) {
 
 		if (typeof panOptions.onPan === 'function') {
 			panOptions.onPan({
-				chartInstance: chartInstance,
+				chart: chartInstance,
 				deltaX: deltaX,
 				deltaY: deltaY
 			});
